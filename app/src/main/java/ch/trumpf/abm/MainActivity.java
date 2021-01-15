3735980e6a60c35881630c469e19433851b89aa9
Elia Scherrer <elia.scherrer@trumpf.com>
2021-01-15 14:32:19 +0100

    Added refresh feature

--- app/src/main/java/ch/trumpf/abm/MainActivity.java
+++ app/src/main/java/ch/trumpf/abm/MainActivity.java
@@ -12,18 +12,19 @@ import android.widget.Button;
 import android.widget.ListView;
 import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Comparator;
+
 import android.provider.ContactsContract;
 
 public class MainActivity extends AppCompatActivity  {
 
     public static final int REQUEST_READ_CONTACTS = 79;
+    public ArrayList<Contact> contacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ArrayList<Contact> contacts;
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED)
         {
@@ -36,10 +37,21 @@ public class MainActivity extends AppCompatActivity  {
 
         Button refresh = (Button) findViewById(R.id.refresh_Btn);
 
+        refresh.setOnClickListener(v ->
+        {
+            contacts.clear();
+            contacts = getAllContacts();
+
+            fillListView(contacts);
+        });
+
+        contacts = sortContacts(contacts);
     }
 
     private void fillListView(ArrayList<Contact> contacts)
     {
+        contacts = sortContacts(contacts);
+
         ArrayList<String> listViewData = new ArrayList<String>();
 
         for (Contact contact : contacts)
@@ -56,6 +68,21 @@ public class MainActivity extends AppCompatActivity  {
 
         contactsListView.setAdapter(arrayAdapter);
     }
+
+    private ArrayList<Contact> sortContacts(ArrayList<Contact> contacts)
+    {
+        Comparator<Contact> contactComparator = new Comparator<Contact>() {
+            @Override
+            public int compare(Contact c1, Contact c2) {
+                return c1.getM_Name().compareToIgnoreCase(c2.getM_Name());
+            }
+        };
+
+        contacts.sort(contactComparator);
+        return contacts;
+    }
+
+
 
     private void requestPermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_CONTACTS)) {
@@ -74,11 +101,13 @@ public class MainActivity extends AppCompatActivity  {
         }
     }
 
-    private ArrayList<Contact> getAllContacts() {
+    private ArrayList<Contact> getAllContacts()
+    {
         ArrayList<Contact> contacts = new ArrayList<Contact>();
         ContentResolver cr = getContentResolver();
         Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-        String columns[] = {
+        String columns[] =
+        {
                 ContactsContract.CommonDataKinds.Event.START_DATE,
                 ContactsContract.CommonDataKinds.Event.TYPE,
                 ContactsContract.CommonDataKinds.Event.MIMETYPE,
@@ -143,6 +172,4 @@ public class MainActivity extends AppCompatActivity  {
         }
         return contacts;
     }
-
-
 }
