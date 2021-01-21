00ece7d86da5c617115b3e1c8815f5a204461d35
Elia Scherrer <elia.scherrer@trumpf.com>
2021-01-21 07:49:02 +0100

    Added EditContact Front-End

--- app/src/main/java/ch/trumpf/abm/EditContact.java
+++ app/src/main/java/ch/trumpf/abm/EditContact.java
@@ -2,20 +2,22 @@ package ch.trumpf.abm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputLayout;
 
-public class EditDefaultActivity extends AppCompatActivity {
+public class EditContact extends AppCompatActivity {
+
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_default);
+        setContentView(R.layout.activity_edit_contact);
 
-
-        Button clear = (Button) findViewById(R.id.EditDefaultClear_Btn);
+        Button clear = (Button) findViewById(R.id.Clear_Btn);
 
         clear.setOnClickListener(v ->
         {
